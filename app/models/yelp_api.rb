be2f42508c68afa7cc2a33edfdcd5cd58a4046c0
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-11-10 14:39:25 +0000

    hid API key in .env file

--- app/models/yelp_api.rb
+++ app/models/yelp_api.rb
@@ -1,24 +1,14 @@
 class YelpApi < ApplicationRecord
 
-    API_KEY = "Q_MKhGJKICh6pddiZIwjIbTTRZBxinR4-GP7xS_Tr6DINyFLAoI11koN7lszLvdKAatcitwFG33E9v8O0UXGApFR5fprBFBtIOv3u77WL0oYZecNNEUnbR06b4LEXXYx"
     API_ENDPOINT = "https://api.yelp.com/v3"
     SEARCH_URL = "#{API_ENDPOINT}/businesses/search"
 
-    def self.generate_business_url(business_id)
-        "#{API_ENDPOINT}/businesses/#{business_id}"
-    end
-
-    def self.generate_reviews_url(business_id)
-        "#{YelpApi.generate_business_url(business_id)}/reviews"
-    end
-
     def self.get_restaurants(location = "london", category = "all")
-        
         return Unirest.get( 
             SEARCH_URL, 
             headers: {
                 "Accept" => "application/json",
-                "Authorization" => "Bearer #{API_KEY}"
+                "Authorization" => "Bearer #{ENV["API_KEY"]}"
             },
             parameters: {
                 :location => location,
@@ -35,7 +25,7 @@ class YelpApi < ApplicationRecord
             business_url, 
             headers: {
                 "Accept" => "application/json",
-                "Authorization" => "Bearer #{API_KEY}"
+                "Authorization" => "Bearer #{ENV["API_KEY"]}"
             }
         ).raw_body
     end
@@ -46,9 +36,17 @@ class YelpApi < ApplicationRecord
             reviews_url, 
             headers: {
                 "Accept" => "application/json",
-                "Authorization" => "Bearer #{API_KEY}"
+                "Authorization" => "Bearer #{ENV["API_KEY"]}"
             }
         ).raw_body
+    end
+
+    def self.generate_business_url(business_id)
+        "#{API_ENDPOINT}/businesses/#{business_id}"
+    end
+
+    def self.generate_reviews_url(business_id)
+        "#{YelpApi.generate_business_url(business_id)}/reviews"
     end
 
 end
