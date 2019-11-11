71efec78c907a5004e4051fd7df5a6d7429b3507
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-11-11 19:41:04 +0000

    validation working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
     def validate
         user = get_current_user
         if user
-          render json: { username: user.username, token: issue_token({ id: user.id }) }
+          render json: { first_name: user.first_name, token: issue_token({ id: user.id }) }
         else
           render json: { error:  'Unable to validate user.' }, status: 401
         end
